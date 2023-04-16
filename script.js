df84cc61fc87c1cc9e88bf2889bba514ac71196f
shagufa01 <shagufanaaz321@gmail.com>
2023-04-16 09:24:54 +0530

    practiced dom

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-function add() {
-    let mainContainer = document.getElementById("container");
+let mainContainer = document.getElementById("container");
+
+function addText() {
     let inputText = document.getElementById("addInput");
     let inputValue = inputText.value;
-    let newElement = document.createElement("p");
-    newElement.innerText = inputValue;
-    mainContainer.appendChild(newElement);
+    let initialText = document.createElement("p");
+    initialText.innerText = inputValue;
+    mainContainer.appendChild(initialText);
+    inputText.value = "";
 
-    let removeElement = newElement.setAttribute("onclick", "removeText(this)");
-    
+    let newDiv = document.createElement("div");
+    mainContainer.appendChild(newDiv);
 
-}
+    let removeEl = document.createElement("button");
+    removeEl.innerText = "❌";
+    newDiv.appendChild(removeEl);
+    removeEl.classList.add("deleteButton");
+    removeEl.addEventListener("click",function(){
+        initialText.remove();
+    })
 
-function removeText(hi) {
-    let mainContainer = document.getElementById("container");
-    mainContainer.removeChild(hi);
+    let completeEl = document.createElement("button");
+    completeEl.innerText = "✔";
+    newDiv.appendChild(completeEl);
+    completeEl.classList.add("completeButton");
+    completeEl.addEventListener("click",function(){
+        if(initialText.classList.contains("complete")) {
+            initialText.classList.remove("complete");
+        }
+        else {
+            initialText.classList.add("complete");
+        }
+    })
+
+    let editEl = document.createElement("button");
+    editEl.innerText = "✏";
+    newDiv.appendChild(editEl);
+    editEl.classList.add("editButton");
+    editEl.addEventListener("click",function(){
+        let newText = document.createElement("input");
+        newText.value = initialText.innerText;
+         newDiv.appendChild(newText);
+        initialText.remove();
+
+        newText.addEventListener("keypress",function(e){
+            if(e.key == "Enter") {
+                let updatedText = document.createElement("p");
+                updatedText.innerHTML = newText.value;
+                newDiv.appendChild(updatedText);
+                newText.remove();
+            }
+        })
+        
+    })
+   
 }
